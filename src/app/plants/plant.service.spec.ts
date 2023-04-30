5d5bf27cf122207121869ffb9b90ec7e4b016933
Santiago Sierra Rassa <s.sierrar2@uniandes.edu.co>
2023-04-30 13:58:10 -0500

    Se crea la prueba del componente y se ajustan los demás archivos de pruebas

--- src/app/plants/plant.service.spec.ts
+++ src/app/plants/plant.service.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlantService } from './plant.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('Service: Plant', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [PlantService]
     });
   });
