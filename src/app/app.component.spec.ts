061ef2f13b65af77073ef211ee7d23b193723cd3
Santiago Sierra Rassa <s.sierrar2@uniandes.edu.co>
2023-04-30 12:44:00 -0500

    Tuning main app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlantsComponent } from './plants/plants.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        PlantsComponent,
+        FooterComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +34,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('MISW-4104-Parcial1');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MISW-4104-Parcial1 app is running!');
-  });
 });
